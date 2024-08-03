e3a30029e01c3fdf6ba2ce19decc0869482fb7eb
Damir Abdullaev <abdullaevdamir34@gmail.com>
2024-08-03 11:20:44 +0300

    Закончил создания и стилизацию Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
 
+        <nav class="nav">
+            <a href="#!" class="logo"><img src="@/assets/img/logo.svg" alt="logo"> <span>vue weather</span></a>
+
+            <div class="nav__search">
+                <img src="@/assets/img/dots.svg" alt="dots">
+                <input type="text" placeholder="Введите название города">
+            </div>
+        </nav>
+
     </div>
 </template>
 
@@ -9,7 +18,3 @@
         
     }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
